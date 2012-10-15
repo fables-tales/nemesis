f981d517bb2dd16060d6b05b86f12f72ebfd0f01
Sam Phippen <samphippen@googlemail.com>
2012-10-15 18:34:00 +0100

    Add tests for `is_teacher_of`
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/test_ldap.py
+++ test/test_ldap.py
@@ -1,6 +1,6 @@
 import unittest
 import helpers
-from serverldap import LdapInstance
+from serverldap import LdapInstance, is_teacher_of
 
 class TestAuth(unittest.TestCase):
 
@@ -9,6 +9,9 @@ class TestAuth(unittest.TestCase):
 
     def test_is_teacher_teacher(self):
         self.assertTrue(LdapInstance("../nemesis/userman").is_teacher("teacher_coll1"))
+
+    def test_is_teacher_of_teacher(self):
+        self.assertTrue(is_teacher_of("../nemesis/userman", "teacher_coll1", "student_coll1_1"))
 
 if __name__ == '__main__':
     unittest.main()
