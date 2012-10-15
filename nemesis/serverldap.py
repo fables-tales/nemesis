6a971b977296c5c4b81802e341de90d61aa989c9
Sam Phippen <samphippen@googlemail.com>
2012-10-15 18:11:58 +0100

    Add a get user details method to the ldap object
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nemesis/serverldap.py
+++ nemesis/serverldap.py
@@ -11,6 +11,19 @@ def get_teachers(userman_path):
     return p.stdout.read().split(" ")
 
 
+def user_details(userman_path, userid):
+    process = ["./userman", "user", "info", userid]
+    p = subprocess.Popen(process, stdout=subprocess.PIPE, cwd=userman_path)
+    p.wait()
+    lines = p.stdout.read().strip().split("\n")
+    results = {}
+    for line in lines:
+        key,value = line.split(": ")
+        results[key] = value
+
+    return results
+
+
 class LdapInstance:
     def __init__(self,userman_path="userman"):
         self.config = ConfigParser.SafeConfigParser()
@@ -24,14 +37,17 @@ class LdapInstance:
 
     def bind(self, username, password):
         bind_str = "uid=" + username + ",ou=users,o=sr"
-        print bind_str
 
         try:
             self.conn.simple_bind_s(bind_str, password)
             self.bound = True
+            print "bound succeeded"
             return True
         except ldap.INVALID_CREDENTIALS:
             return False
+
+    def get_user_details(self,userid):
+        return user_details(self.userman_path, userid)
 
 
 if __name__ == "__main__":
