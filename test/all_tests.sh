5cf09a98ad53a3deffe67750e369a7fd1d38fec9
Sam Phippen <samphippen@googlemail.com>
2012-10-23 01:11:27 +0100

    Make all tests more robust
    the sleep is now longer and it self activates the venv
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/all_tests.sh
+++ test/all_tests.sh
@@ -1,6 +1,6 @@
-cd ../nemesis
-python app.py 1>/dev/null 2>/dev/null &
-sleep 0.3
+source ../venv/bin/activate
+python ../nemesis/app.py 1>/dev/null 2>/dev/null &
+sleep 0.4
 cd ../test
 python -m unittest discover
 killall python
