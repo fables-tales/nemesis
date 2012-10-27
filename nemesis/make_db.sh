893dfb183f3a56b8f2763bff3eb421d02761e405
Sam Phippen <samphippen@googlemail.com>
2012-10-27 18:17:29 +0100

    Add the code to make the registration table
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nemesis/make_db.sh
+++ nemesis/make_db.sh
@@ -1,1 +1,7 @@
 sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS auth (token VARCHAR(256), username VARCHAR(256))"
+sqlite3 db/nemesis.sqlite "CREATE table IF NOT EXISTS registrations (id INTEGER PRIMARY KEY ASC AUTOINCREMENT,\
+                                                                     teacher_username VARCHAR(256),\
+                                                                     college_group    VARCHAR(256),\
+                                                                     first_name       VARCHAR(256),\
+                                                                     last_name        VARCHAR(256),\
+                                                                     email            VARCHAR(256));"
