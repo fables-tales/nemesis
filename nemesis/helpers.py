2330603fad3c76e5251f6e4e795bd034c9ffee11
Sam Phippen <samphippen@googlemail.com>
2012-10-27 18:16:56 +0100

    Add some helpers
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ nemesis/helpers.py
@@ -0,0 +1,18 @@
+import sqlite3
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+def sqlite_connect():
+    return sqlite3.connect(PATH + "/db/nemesis.sqlite")
+
+def register_user(teacher_username, college_group, first_name, last_name, email):
+    conn = sqlite_connect()
+    cur = conn.cursor()
+    prep_statement = "INSERT INTO registrations (teacher_username, college_group, first_name, last_name, email) VALUES (?,?,?,?,?)";
+    cur.execute(prep_statement, (teacher_username, college_group, first_name, last_name, email))
+    conn.commit()
+
+
+
+
