5cc4bef63cb23426a10acd60f0c783fc15475486
Sam Phippen <samphippen@googlemail.com>
2012-11-05 16:48:38 +0000

    Add a dumper for the registrations and a .gitignore for the csv
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ nemesis/dump_db.py
@@ -0,0 +1,21 @@
+import helpers
+import csv
+
+if __name__ == "__main__":
+    conn = helpers.sqlite_connect()
+    cur  = conn.cursor()
+    ids = set()
+    details = []
+
+    for row in cur.execute("SELECT * FROM registrations WHERE processed='0'"):
+        details.append(row)
+        ids.add(row[0])
+
+    for id in ids:
+        cur.execute("UPDATE registrations SET processed='1' WHERE id=?", (id,))
+        conn.commit()
+
+    with open("users.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        for row in details:
+            writer.writerow(row)
