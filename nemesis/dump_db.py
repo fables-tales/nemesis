58ad8be8bef8e60583d422e8cbd60d33d5da56fb
Sam Phippen <samphippen@googlemail.com>
2012-11-22 12:26:24 -0800

    Explain the two columns that aren't utf8 encoded
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nemesis/dump_db.py
+++ nemesis/dump_db.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             #the format of the row is such that the first and last entries are
             #integers and everything else is a string. Given that the strings
             #come from user input they should be utf-8 encoded to make them
-            #work
+            #work. The other two are id and processed (boolean 1 or 0)
             for i in range(1,7):
                 row[i] = row[i].encode("utf-8")
             writer.writerow(row)
