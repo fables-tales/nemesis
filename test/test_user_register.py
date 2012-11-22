ba2f2fe95379e156beb1acd4aaca9847bc3c2688
Sam Phippen <samphippen@googlemail.com>
2012-11-22 10:25:28 -0800

    Add some tests for the db dumping script
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/test_user_register.py
+++ test/test_user_register.py
@@ -2,6 +2,7 @@ import unittest
 import json
 import test_helpers as helpers
 from copy import copy
+import subprocess
 
 
 class TestAuth(unittest.TestCase):
@@ -10,6 +11,7 @@ class TestAuth(unittest.TestCase):
         resp_auth = helpers.server_post("/auth", {"username":"teacher_coll2", "password":"noway"})
         self.auth_hash = json.loads(resp_auth.read())["token"]
         self.user_hash = {"first_name": "bob", "last_name":"monkey", "email":"mail@mail.com", "team":"team1"}
+        self.user_hash_unicode = {"first_name": u"b\u00F6b", "last_name":"monkey", "email":"mail@mail.com", "team":"team1"}
 
     def test_no_authentication(self):
         result = helpers.server_post("/user/register", self.user_hash)
@@ -28,6 +30,32 @@ class TestAuth(unittest.TestCase):
         helpers.server_post("/user/register", local_hash)
         print helpers.get_registrations()
         self.assertEqual(len(helpers.get_registrations()), 1)
+
+
+    def test_dump_db_ascii(self):
+        local_hash = copy(self.user_hash)
+        local_hash["token"] = self.auth_hash
+        helpers.server_post("/user/register", local_hash)
+        p = subprocess.Popen("../nemesis/dump_db.py")
+        p.wait()
+        users_csv = open("users.csv").read()
+        self.assertTrue("bob" in users_csv)
+        self.assertTrue("monkey" in users_csv)
+        self.assertTrue("mail@mail.com" in users_csv)
+        self.assertTrue("team1" in users_csv)
+
+    def test_dump_db_unicode(self):
+        local_hash = copy(self.user_hash_unicode)
+        local_hash["token"] = self.auth_hash
+        helpers.server_post("/user/register", local_hash)
+        p = subprocess.Popen("../nemesis/dump_db.py")
+        p.wait()
+        users_csv = open("users.csv").read()
+        self.assertTrue(u"b\u00F6b" in users_csv.decode("utf-8"))
+        self.assertTrue("monkey" in users_csv)
+        self.assertTrue("mail@mail.com" in users_csv)
+        self.assertTrue("team1" in users_csv)
+
 
     def tearDown(self):
         deauth = helpers.server_post("/deauth", {"token":self.auth_hash})
