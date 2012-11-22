52b20af4ca9da51c22d603b22cf1b75b416c4175
Sam Phippen <samphippen@googlemail.com>
2012-11-22 13:27:49 -0800

    Add some more required deps to the selenium deps script
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/client-tests/get-dependencies.sh
+++ test/client-tests/get-dependencies.sh
@@ -1,3 +1,6 @@
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.26.0.jar
-yum groupinstall 'Fonts'
-yum install 'firefox'
+wget -c http://selenium.googlecode.com/files/selenium-server-standalone-2.26.0.jar
+yum groupinstall -y 'Fonts'
+yum install -y 'firefox'
+yum install -y xorg-x11-server-Xvfb
+sudo easy_install pyvirtualdisplay
+sudo easy_install selenium
