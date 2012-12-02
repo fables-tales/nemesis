6cc4038a393ddee7c383cdd2dfa6422e56141848
Sam Phippen <samphippen@googlemail.com>
2012-12-02 15:42:55 -0800

    Make the all tests script work on my badger clone
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- test/api-tests/all_tests.sh
+++ test/api-tests/all_tests.sh
@@ -1,6 +1,5 @@
-source ../../venv/bin/activate
-python ../../nemesis/app.py 1>/dev/null 2>/dev/null &
-sleep 0.4
+python ../../nemesis/app.py&
+sleep 2
 python -m unittest discover
 killall python
 
