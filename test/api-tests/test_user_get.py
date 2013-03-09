680d2f70697f8c78830e0e53ec0e6539f989152e
Sam Phippen <samphippen@googlemail.com>
2013-03-09 10:11:00 -0800

    Add some tests for the GET /user/:userid method
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ test/api-tests/test_user_get.py
@@ -0,0 +1,34 @@
+import test_helpers
+import sys
+
+sys.path.append("../../nemesis/libnemesis")
+from libnemesis import User
+
+def test_user_get_no_user():
+    params = {}
+    r,data = test_helpers.server_get("/user/student_coll1_1", params)
+    assert r.status == 403
+
+def test_user_get_wrong_user():
+    params = {"username":"teacher_coll2",
+              "password":"noway",
+              }
+
+    r,data = test_helpers.server_get("/user/student_coll1_1", params)
+    assert r.status == 403
+
+def test_user_get_self():
+    params = {"username":"student_coll1_1",
+              "password":"cows"}
+    r,data = test_helpers.server_get("/user/student_coll1_1", params)
+    assert r.status == 200
+    assert data.find("student_coll1_1") != -1
+
+def test_user_get_other_can_view():
+    params = {"username":"blueshirt",
+              "password":"blueshirt",
+              }
+
+    r,data = test_helpers.server_get("/user/student_coll1_1", params)
+    assert r.status == 200
+    assert data.find("student_coll1_1") != -1
