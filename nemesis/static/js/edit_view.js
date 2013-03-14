bb997d56894a9460ab2996c7cf9aa325eac7ba6f
Sam Phippen <samphippen@googlemail.com>
2013-03-14 07:46:56 -0700

    Add a code to submit the user details from the edit form
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- nemesis/static/js/edit_view.js
+++ nemesis/static/js/edit_view.js
@@ -1,20 +1,58 @@
 var EditView = function() {
-    return function(jquerynode) {
+    return function(jquerynode, refresh_callback) {
         var jquerynode = jquerynode;
+        var that = this;
+        var my_user;
 
         this.show = function(username) {
-            var u = new User(username);
-            u.fetch(function(user) {
+            my_user = new User(username);
+            this.refresh_view();
+        };
+
+        this.refresh_view = function() {
+            my_user.fetch(function(user) {
                 var text = TemplateExpander.template("user_edit").render_with({"user":user});
                 console.log(text);
                 console.log(jquerynode);
                 jquerynode.html(text);
                 jquerynode.show();
+                wv.end("Loaded user successfully!");
+                $("#edit-submit").click(function() {
+                    that.submit_form();
+                });
+
+                refresh_callback();
             });
         };
 
         this.hide = function() {
             jquerynode.hide();
         };
+
+        this.submit_form = function() {
+            wv.start("Sending user details");
+            $.post("user/" + my_user.username, this.details_on_form(), function(response) {
+                that.refresh_view();
+            });
+        };
+
+        this.details_on_form = function() {
+            var details = {};
+            if (password_input() != "") {
+                details["new_password"] = password_input();
+            }
+
+            $("#update-user").find("input[type=text]").each(function(i,element) {
+                var $e = $(element);
+                details[$e.attr("name")] = $e.val();
+            });
+
+            return details;
+        };
+
+        var password_input = function() {
+            return $("input[name=new_password]").val();
+        };
+
     };
 }();
