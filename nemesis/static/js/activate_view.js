b0332d04a507013ab533a4da40367d040ee7f65c
Peter Law <PeterJCLaw@gmail.com>
2013-10-02 21:18:07 +0100

    Have users directly set a new password on the activation page.
    Replace the static page which told users to go somewhere else to
    change their password with a dynamic page that feels much more like
    the rest of usreman. This page contains a password setting form that
    ensures the password is long enough (6 chars) and matches a confirmation
    field. After submission, the user is then shown a similar message
    which confirms their details and links them to the forums etc.
    
    Change-Id: I79254cbd0752ab67c128fa916643d35edb3e1aeb

---
+++ nemesis/static/js/activate_view.js
@@ -0,0 +1,58 @@
+var ActivateView = function() {
+    return function(jquerynode, user) {
+        var jquerynode = jquerynode;
+        var that = this;
+        var my_user = user;
+
+        this.show = function(user) {
+            var template = TemplateExpander.template("set-password");
+            var opts = { "first_name": user.first_name };
+            var text = template.render_with(opts);
+            jquerynode.html(text);
+            jquerynode.show();
+            $("#set-password-submit").click(function() {
+                that.submit_form(user.username);
+            });
+        };
+
+        this.submit_form = function(username) {
+            //wv.start("Sending user details");
+            var details = this.details_on_form();
+            if (!details) {
+                return;
+            }
+            $.post("user/" + my_user.username, details, function(response) {
+                that.show_completion();
+            });
+        };
+
+        this.details_on_form = function() {
+            var password_in = $("input[name=new_password]").val();
+            var confirm_password = $("input[name=confirm_password]").val();
+
+            if (password_in != confirm_password) {
+                error("Passwords don't match.");
+                return false;
+            }
+
+            if (password_in.length < 6) {
+                error("Password too short.");
+                return false;
+            }
+
+            return { 'new_password': password_in };
+        };
+
+        this.show_completion = function() {
+            var template = TemplateExpander.template("account-details");
+            var text = template.render_with(my_user);
+            jquerynode.html(text);
+            jquerynode.show();
+        }
+
+        var error = function(text) {
+            // TODO
+            alert(text);
+        }
+    };
+}();
