73270d5f38e032c6aa4abd491ad0c190517d3e12
Peter Law <PeterJCLaw@gmail.com>
2013-10-05 14:25:44 +0100

    Prevent registrations from students.
    Previously, due to a bug in libnemesis, any authenticated used could
    register new users, contrary to the intent of the code and the spec.
    Bump libnemesis to pick up the fix, and add tests to ensure that the
    desired functionality is indeed present in nemesis.
    
    Change-Id: I747c39eb1969abecc653d81b544c24c0208d5533

--- test/api-tests/test_registrations.py
+++ test/api-tests/test_registrations.py
@@ -15,6 +15,22 @@ def test_registration_user_and_form():
               "college":"college-1"}
 
     r,data = test_helpers.server_post("/registrations", params)
+    status = r.status
+    assert status == 202
+    assert len(test_helpers.get_registrations()) == 1
+    test_helpers.delete_db()
+
+def test_registration_user_and_form():
+    test_helpers.delete_db()
+    params = {"username":"blueshirt",
+              "password":"blueshirt",
+              "first_name":"register",
+              "last_name":"this.user",
+              "email":"bob@example.com",
+              "team":"team-ABC",
+              "college":"college-1"}
+
+    r,data = test_helpers.server_post("/registrations", params)
     assert r.status == 202
     assert len(test_helpers.get_registrations()) == 1
     test_helpers.delete_db()
@@ -31,6 +47,44 @@ def test_registration_wrong_college():
               "college":"college-2"}
 
     r,data = test_helpers.server_post("/registrations", params)
-    assert r.status == 403
+
+    status = r.status
+    assert status == 403
+    assert 'BAD_COLLEGE' in data
+    assert len(test_helpers.get_registrations()) == 0
+    test_helpers.delete_db()
+
+def test_registration_not_authed():
+    test_helpers.delete_db()
+
+    params = {"username":"teacher_coll1",
+              "first_name":"register",
+              "last_name":"this.user",
+              "email":"bob@example.com",
+              "team":"team-ABC",
+              "college":"college-1"}
+
+    r,data = test_helpers.server_post("/registrations", params)
+    status = r.status
+    assert status == 403
+    assert 'NO_PASSWORD' in data
+    assert len(test_helpers.get_registrations()) == 0
+    test_helpers.delete_db()
+
+def test_registration_rq_from_student():
+    test_helpers.delete_db()
+
+    params = {"username":"student_coll1_1",
+              "password":"cows",
+              "first_name":"register",
+              "last_name":"this.user",
+              "email":"bob@example.com",
+              "team":"team-ABC",
+              "college":"college-1"}
+
+    r,data = test_helpers.server_post("/registrations", params)
+    status = r.status
+    assert status == 403
+    assert 'YOU_CANT_REGISTER_USERS' in data
     assert len(test_helpers.get_registrations()) == 0
     test_helpers.delete_db()
