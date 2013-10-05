1557aaa09cbc51743cccf70652c5b353c29df07c
Peter Law <PeterJCLaw@gmail.com>
2013-10-05 18:40:23 +0100

    Remove obsolete function.
    This could have been removed in 621b6c2275ec2cf500 but was missed.
    
    Change-Id: Ie9d24bb81c22abb283048806473e5ee4588cc7e8

--- nemesis/helpers.py
+++ nemesis/helpers.py
@@ -10,12 +10,6 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 def sqlite_connect():
     return sqlite3.connect(PATH + "/db/nemesis.sqlite")
 
-def _exec(statement, arguments):
-    conn = sqlite_connect()
-    cur = conn.cursor()
-    cur.execute(statement, arguments)
-    conn.commit()
-
 def create_verify_code(username, new_email):
     """
     An increadibly weak way of generating a 'random' looking string which
